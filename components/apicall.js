78eef45755d11724816e9e4bbe4e63cb5c015f8b
akaboxmac <akabox@akabox.akabox>
2022-04-17 04:18:33 +0300

    add basic logic

---
+++ components/apicall.js
@@ -0,0 +1,10 @@
+module.exports = {
+    callbridge: function () {
+        // func1 impl
+        console.log("call bridge");
+    },
+    callgame: function () {
+        // func2 impl
+        console.log("call game");
+    }
+};
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const apicall = require('./components/apicall')
 const express = require('express')
 const path = require('path')
 const cool = require('cool-ascii-faces');
 const PORT = process.env.PORT || 3000
 
-const { Pool } = require('pg');
+//database related
+const {Pool} = require('pg');
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
@@ -11,7 +13,31 @@ const pool = new Pool({
     }
 });
 
-console.log("service started ");
+
+//init parameters
+let para = -1;
+
+
+const requestloop = setInterval(function () {
+    if (para === "game") {
+        apicall.callgame();
+    } else if (para === "bridge") {
+        apicall.callbridge();
+    }
+}, 1000);
+
+//start the loop
+console.log(requestloop);
+
+
+function paraswitch() {
+    if (para === "game") {
+        para = "bridge";
+    } else {
+        para = "game";
+    }
+}
+
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
@@ -21,13 +47,20 @@ express()
         res.render('pages/index')
         // console.log("loaded main page");
     })
-    .get('/cool', (req, res) => res.send(cool()))
+    .get('/cool', (req, res) => {
+        res.send(cool())
+    })
+    .get('/switch', (req, res) => {
+
+        paraswitch();
+        res.send("switch to " + para);
+    })
     .get('/db', async (req, res) => {
         try {
             const client = await pool.connect();
             const result = await client.query('SELECT * FROM test_table');
-            const results = { 'results': (result) ? result.rows : null};
-            res.render('pages/db', results );
+            const results = {'results': (result) ? result.rows : null};
+            res.render('pages/db', results);
             client.release();
         } catch (err) {
             console.error(err);
