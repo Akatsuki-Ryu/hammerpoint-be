ecb475511bc85da9805d3d13ddcfd0d85b3923fc
akaboxmac <akabox@akabox.akabox>
2022-04-19 17:47:54 +0300

    add write to userlist json

--- components/usermanagement.js
+++ components/usermanagement.js
@@ -1,11 +1,28 @@
+const {writetofile, readfromfile} = require("./datamgr");
 let userlist = [
-    {playername: "akabox218",uid:"1007820601979"},
-    {playername: "terpko",uid:"1007820601979"},
-    {playername: "hakipi",uid:"2545398846"},
-    {playername: "TheGTRacer97",uid:"2299827182"},
-    {playername: "The9axel5",uid:"2298814335"}
+    {playername: "akabox218", uid: "1007820601979"},
+    {playername: "terpko", uid: "1007820601979"},
+    {playername: "hakipi", uid: "2545398846"},
+    {playername: "TheGTRacer97", uid: "2299827182"},
+    {playername: "The9axel5", uid: "2298814335"}
 
 
 ]
-let usertestval = 80;
-module.exports = {userlist,usertestval};
+let userlistinitflag = 0;
+let userdatapath = "./public/userlist";
+
+function userlistinit() {
+    // console.log(readfromfile(userdatapath+"-undefined.json"));
+    if (userlistinitflag === 0 && readfromfile(userdatapath+"-undefined.json")) {
+
+        writetofile(userdatapath, userlist);
+        console.log("written userlist =====================");
+        userlistinitflag = 1;
+    }
+
+}
+
+userlistinit();
+// writetofile(userdatapath, userlist);
+
+module.exports = {userlist};
