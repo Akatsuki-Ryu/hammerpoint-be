d6890c02eb2a4bf9e8cd580735b3f9168c5bb881
akaboxmac <akabox@akabox.akabox>
2022-04-24 18:24:50 +0300

    add playerlist on serverside

--- components/usermanagement.js
+++ components/usermanagement.js
@@ -2,14 +2,16 @@ const {writetofile, readfromfile} = require("./datamgr");
 const {callbridge} = require("./apicall");
 
 let playerlist = [
-    {playername: "akabox218", uid: "1007820601979", ingame: 0,needcallgame:0,highrequesttimestamp:0},
-    {playername: "terpko", uid: "2297370779", ingame: 0,needcallgame:0,highrequesttimestamp:0},
-    {playername: "hakipi", uid: "2545398846", ingame: 0,needcallgame:0,highrequesttimestamp:0},
-    {playername: "TheGTRacer97", uid: "2299827182", ingame: 0,needcallgame:0,highrequesttimestamp:0},
-    {playername: "The9axel5", uid: "2298814335", ingame: 0,needcallgame:0,highrequesttimestamp:0},
-    {playername: "Lundaki", uid: "2381075935", ingame: 0,needcallgame:0,highrequesttimestamp:0}
+    {profilename: "akabox", "profilephoto": "https://cdn.discordapp.com/avatars/215068545552351250/5a600bbca2331678b276125998c16ecc.png?size=1024",playername: "akabox218", uid: "1007820601979", ingame: 0,needcallgame:0,highrequesttimestamp:0},
+    {profilename: "terpko", "profilephoto": "https://cdn.discordapp.com/avatars/171358872684986368/cc87dcbae8d37e8b5aed2e8da34d4e71.png?size=1024",playername: "terpko", uid: "2297370779", ingame: 0,needcallgame:0,highrequesttimestamp:0},
+    {profilename: "hakipi", "profilephoto": "https://cdn.discordapp.com/avatars/284391184393306112/a390ffac93ac94f7a91fbdea0057cc1e.png?size=1024",playername: "hakipi", uid: "2545398846", ingame: 0,needcallgame:0,highrequesttimestamp:0},
+    {profilename: "GT", "profilephoto": "https://cdn.discordapp.com/avatars/127868875810406400/ba92a07ff12d732b24c9d7c57478b621.png?size=1024",playername: "TheGTRacer97", uid: "2299827182", ingame: 0,needcallgame:0,highrequesttimestamp:0},
+    {profilename: "serveri","profilephoto": "https://cdn.discordapp.com/avatars/222299113713041408/847fbea14b00f3a87385fd7912313948.png?size=1024",playername: "The9axel5", uid: "2298814335", ingame: 0,needcallgame:0,highrequesttimestamp:0},
+    {profilename: "Maidaki","profilephoto": "https://cdn.discordapp.com/avatars/251744309735456768/d5122ec107ac109880460b5252c454e6.png?size=1024",playername: "Lundaki", uid: "2381075935", ingame: 0,needcallgame:0,highrequesttimestamp:0}
 
 ];
+
+
 
 let highdemandlist = [];
 
