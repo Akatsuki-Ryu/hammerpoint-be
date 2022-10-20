dbcd3bea1cd06cf7ec46019d65ccb30588835c0c
akaboxmac <akabox@akabox.akabox>
2022-10-20 14:06:30 +0300

    remove wake up dyno test2travel

--- index.js
+++ index.js
@@ -170,6 +170,6 @@ express()
     .listen(PORT, () => {
         console.log(`Listening on ${PORT}` + "  running on " + process.env.ENVVAL);
         wakeUpDyno(process.env.DYNO_URL);
-        wakeUpDyno(process.env.DYNO_URL_AVC, 1);
+        // wakeUpDyno(process.env.DYNO_URL_AVC, 1);
 
     });
